23d3383525f441035482e960e2c2ee58ed7a8333
maheepisher <55838286+maheepisher@users.noreply.github.com>
2025-03-31 17:50:07 -0400

    Update user_routes.py

--- Users API/app/routes/user_routes.py
+++ Users API/app/routes/user_routes.py
@@ -7,7 +7,7 @@ user_routes = Blueprint('user_routes', __name__)
 
 @user_routes.route('/user/test', methods=['GET'])
 def test():
-    return jsonify({'Message': 'This API is working!'})
+    return jsonify({'Message': 'This API is Works!'})
 
 # --------------------------- GET User Details ---------------------------
 @user_routes.route('/user', methods=['GET'])
