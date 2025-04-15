5dfd728169598ff07ef86bbd238bad2dc21893fe
maheepisher <maheep.isher@gmail.com>
2025-04-15 18:32:28 -0400

    okayy

--- Reservation API/app/utils/db_connection.py
+++ Reservation API/app/utils/db_connection.py
@@ -9,8 +9,8 @@ def get_db_connection():
     try:
         connection = mysql.connector.connect(
             host=os.getenv('DB_HOST'),
-            user=os.getenv('DB_USER'),
-            password=os.getenv('DB_PASSWORD'),
+            user=os.getenv('DBUSER'),
+            password=os.getenv('DBPASSWORD'),
             database=os.getenv('DB_NAME')
         )
         if connection.is_connected():
