92337361311cc7ce04a501da3b7c19f5e577de79
maheepisher <maheep.isher@gmail.com>
2025-04-17 19:08:49 -0400

    okay corss

--- Books API/run.py
+++ Books API/run.py
@@ -9,15 +9,7 @@ app = Flask(__name__)
 #CORS(app)
 #CORS(app, resources={r"/api/*": {"origins": ["http://capstone-lms-frontend-bucket.s3-website-us-east-1.amazonaws.com"]}})
 # Register your blueprint
-
-@app.after_request
-def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = 'http://capstone-lms-frontend-bucket.s3-website-us-east-1.amazonaws.com'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Accept, Authorization'
-    response.headers['Access-Control-Allow-Credentials'] = 'true'  # If credentials are needed
-    return response
-
+CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
 app.register_blueprint(books_routes, url_prefix='/api')
 
 if __name__ == '__main__':
