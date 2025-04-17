415066805649e0734330256b552bfabf5e76a941
maheepisher <maheep.isher@gmail.com>
2025-04-17 18:25:35 -0400

    okayy cors

--- Reservation API/run.py
+++ Reservation API/run.py
@@ -7,10 +7,20 @@ app = Flask(__name__)
 
 # Enable CORS
 #CORS(app)
-CORS(app, resources={r"/api/*": {"origins": ["http://capstone-lms-frontend-bucket.s3-website-us-east-1.amazonaws.com"]}})
+#CORS(app, resources={r"/api/*": {"origins": ["http://capstone-lms-frontend-bucket.s3-website-us-east-1.amazonaws.com"]}})
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = 'http://capstone-lms-frontend-bucket.s3-website-us-east-1.amazonaws.com'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Accept, Authorization'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'  # If credentials are needed
+    return response
 
 # Register your blueprint
 app.register_blueprint(reservation_routes, url_prefix='/api')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Users API/run.py
+++ Users API/run.py
@@ -7,8 +7,17 @@ app = Flask(__name__)
 
 # Enable CORS
 #CORS(app)
-CORS(app, resources={r"/api/*": {"origins": ["http://capstone-lms-frontend-bucket.s3-website-us-east-1.amazonaws.com"]}})
+#CORS(app, resources={r"/api/*": {"origins": ["http://capstone-lms-frontend-bucket.s3-website-us-east-1.amazonaws.com"]}})
 # Register your blueprint
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = 'http://capstone-lms-frontend-bucket.s3-website-us-east-1.amazonaws.com'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Accept, Authorization'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'  # If credentials are needed
+    return response
+
 app.register_blueprint(user_routes, url_prefix='/api')
 
 if __name__ == '__main__':
